6a123c07d0153133ef928d2214ef58e5ea9f34a2
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-11-18 12:18:19 +0100

    porcoddio

---
+++ RicorsivitàEs.js
@@ -0,0 +1,11 @@
+//chiedere a saro
+
+function isEven(x){
+    return (x%2 == 0) ? 0 : 1
+    // if((x%2) == 0 || x == (x-2)){
+    //     return 0
+    // }
+    // else return 1
+}
+
+console.log(isEven(34))
